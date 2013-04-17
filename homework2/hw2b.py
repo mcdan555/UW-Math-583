1789f934afb886433cf161e56da1844fb5d92b94
Daniel McInally <dhm@uw.edu>
2013-04-17 16:09:08 -0700

    added the cubic fuctions

--- homework2/hw2b.py
+++ homework2/hw2b.py
@@ -66,6 +66,58 @@ def plot_quad (xi,yi):
 
         plt.savefig('quadratic.png')
 
+def cubic_interp(xi,yi):
+        """
+        Quadratic interpolation.  Compute the coefficients of the polynomial
+        interpolating the points (xi[i],yi[i]) for i = 0,1,2,3.
+        Returns c, an array containing the coefficients of
+        p(x) = c[0] + c[1]*x + c[2]*x**2 + c[3]*x**3
+
+        """
+
+        # Error handling of input parameters
+
+        error_message = "xi and yi should have type numpy.ndarray"
+        assert (type(xi) is np.ndarray) and (type(yi) is np.ndarray), error_message
+
+        error_message = "xi and yi should have length 4"
+        assert len(xi)==4 and len(yi)==4, error_message
+
+
+        # Set up the linear system
+
+        A = A = np.vstack([np.ones(4), xi, xi**2, xi**3]).T
+        b = yi
+
+
+        #solving the system
+        c = solve(A,b)
+
+        return c
+
+def plot_cubic (xi,yi):
+        """
+        Imputs two arrays length 3, computes the coefficient
+        matrix from the interpolation, and saves a plot file
+        cubic.png
+        """
+
+        c = cubic_interp(xi,yi)
+
+        # Plot the polynomial
+        x = np.linspace(xi.min() - 1,  xi.max() + 1, 1000)
+        y = c[0] + c[1]*x + c[2]*x**2 + c[3]*x**3
+        
+        plt.figure(1)
+        plt.clf()
+        plt.plot(x,y,'b-')
+        
+        plt.plot(xi,yi, 'ro')
+        plt.ylim(yi.min()-1,yi.max()+1)
+        
+        plt.title("Data points and interpolating polynomial")
+
+        plt.savefig('cubic.png')
         
 # ------------------------------------------------
 
@@ -77,6 +129,34 @@ def test_quad1():
         yi = np.array([ 1., -1.,  7.])
         c = quad_interp(xi,yi)
         c_true = np.array([-1.,  0.,  2.])
+        print "c =      ", c
+        print "c_true = ", c_true
+        # test that all elements have small error:
+        assert np.allclose(c, c_true), \
+                "Incorrect result, c = %s, Expected: c = %s" % (c,c_true)
+
+def test_quad2():
+        """
+        Test code, no return value or exception if test runs properly.
+        """
+        xi = np.array([-5.,  1.,  4.])
+        yi = np.array([ 15., 3.,  24.])
+        c = quad_interp(xi,yi)
+        c_true = np.array([0.,  2.,  1.])
+        print "c =      ", c
+        print "c_true = ", c_true
+        # test that all elements have small error:
+        assert np.allclose(c, c_true), \
+                "Incorrect result, c = %s, Expected: c = %s" % (c,c_true)
+
+def test_cubic1():
+        """
+        Test code, no return value or exception if test runs properly.
+        """
+        xi = np.array([ 0.,  1.,  -1., -3.])
+        yi = np.array([ 5., 9., 5., -7.])
+        c = cubic_interp(xi,yi)
+        c_true = np.array([5.,  1.,  2., 1.])
         print "c =      ", c
         print "c_true = ", c_true
         # test that all elements have small error:
